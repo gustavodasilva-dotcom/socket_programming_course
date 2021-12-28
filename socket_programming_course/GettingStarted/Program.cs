cb414797a47c6dafd21cf48bd36e2d68b694c8e2
Gustavo da Silva <gustavaquinooficial@gmail.com>
2021-12-28 12:25:47 -0300

    Ambientação com criação de um socket.

---
+++ socket_programming_course/GettingStarted/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace GettingStarted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Este é o server-side.
+
+                /**
+                 * Instanciando o socket. Nesse construtor, especificado que a conexão do IP é baseada no IPV-4; em seguida,
+                 * especificado que o tipo de conexão é um stream; por último, o protocolo é TCP.
+                 */
+                var listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+                // Especificando que o IP será "qualquer um"; especificamente, o IP 127.0.0.1 (vulgo localhost).
+                var ipAddress = IPAddress.Any;
+
+                // Instanciando o endpoint, que é a junção do endereço IP + a porta.
+                var ipEndPoint = new IPEndPoint(ipAddress, 23000);
+
+                listenerSocket.Bind(ipEndPoint);
+                listenerSocket.Listen(5);
+                listenerSocket.Accept();
+            }
+            catch (Exception e)
+            {
+                Console.Write($"An error occurred: {e.Message}");
+            }
+        }
+    }
+}
---
+++ socket_programming_course/README.md
@@ -0,0 +1,5 @@
+# socket_programming_course
+
+- Este repositório está sendo criado para publicar os exercícios e aplicações que serão criadas no curso **TCP/IP Socket Programming in C# .Net For Coders & Students** (link para acesso: https://www.udemy.com/course/tcpip-socket-programming-for-coders-using-csharp-net/).
+
+- This repository is being created to publish the exercises and the applications that will be created at the course **TCP/IP Socket Programming in C# .Net For Coders & Students** (link para acesso: https://www.udemy.com/course/tcpip-socket-programming-for-coders-using-csharp-net/).
