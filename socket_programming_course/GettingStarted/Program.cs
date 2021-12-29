40dfea8e997ffc8b9db4becfdd7eea3d7057fc22
Gustavo da Silva <gustavaquinooficial@gmail.com>
2021-12-29 11:38:25 -0300

    Receiving data from the client.

--- socket_programming_course/GettingStarted/Program.cs
+++ socket_programming_course/GettingStarted/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using System.Net;
 using System.Net.Sockets;
 
@@ -26,7 +27,37 @@ namespace GettingStarted
 
                 listenerSocket.Bind(ipEndPoint);
                 listenerSocket.Listen(5);
-                listenerSocket.Accept();
+
+                Console.WriteLine("Prestes a aceitar conexão: ");
+
+                /**
+                 * O método Accept() retorna um socket -- que, no caso, é o cliente. Um cliente, para se comunicar com socket
+                 * no server-side, precisa, também, abrir um socket (informações retiradas do blog:
+                 * https://blog.pantuza.com/artigos/o-que-sao-e-como-funcionam-os-sockets). Então, quando o cliente faz a
+                 * comunicação com o socket do server-side, a aplicação do server-side utilizada o Accept() para ser comunicar.
+                 * No caso do C#, o método Accept() retorna, justamente, o cliente que está fazendo a "requisição" e a conexão.
+                 */
+                var client = listenerSocket.Accept();
+
+                Console.WriteLine($"Client conectado: {client.ToString()}.");
+
+                Console.WriteLine($"IP EndPoint: {client.RemoteEndPoint.ToString()}.");
+
+                var buff = new byte[128];
+
+                /**
+                 * O método Receive() recebe informações através do socket (informações do cliente). Esse método recebe, como
+                 * parâmetro, um array de bytes -- que, nesse caso, funcionará como um parâmetro de tipo de referência. Ou seja,
+                 * o método irá preencher o array de bytes com os dados recebidos do cliente através do socket (os dados são
+                 * em binários).
+                 */
+                var numReceivedBytes = client.Receive(buff);
+
+                Console.WriteLine($"Número de bytes recebidos: {numReceivedBytes}");
+
+                var receivedText = Encoding.ASCII.GetString(buff, 0, numReceivedBytes);
+
+                Console.WriteLine($"Dados recebidos: {receivedText}");
             }
             catch (Exception e)
             {
